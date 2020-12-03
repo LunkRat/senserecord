d3a6eec2702b64bb3dd61115c14f60210452857a
Link Swanson <link@swanson.link>
2020-12-03 17:12:32 -0600

    add a basic confirmation that the data saved to file after recording has finished.

--- senserecord/gui/widgets.py
+++ senserecord/gui/widgets.py
@@ -144,6 +144,14 @@ class OnOffWidget(QWidget):
             # Switch the button to OFF state:
             self.is_on = False
             self.update_button_state()
+            QMessageBox.information(self, "Recording saved!",
+                "<b>Data saved to:</b> \n" 
+                + self.recorder.data_file_base 
+                + ".csv"
+                + "\n <b>Located at:</b> \n"
+                + self.recorder.data_path, 
+                QMessageBox.Ok
+            )
         except Exception:
             logging.exception("Failed to stop stream! Full stack trace:")
 
